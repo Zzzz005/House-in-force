366672bf5d6bc0e3a34df51b8695e0d83af6d3c7
Zhansaya Aralkhan <aralkhanzhansaya@gmail.com>
2024-09-01 17:35:56 +0500

    :wq

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('#menu');
 coffee.addEventListener('click', () => {
     menu.classList.toggle('disp')
 });
+
+document.querySelector("#show-popup").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector("#close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+})
+
